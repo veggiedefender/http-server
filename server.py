57634c4d74571d4f0f014a483ac48e83d1bc20a8
Jesse Li <jessejesse123@gmail.com>
2018-05-07 20:47:56 -0400

    parse request into object

--- server.py
+++ server.py
@@ -12,8 +12,27 @@ HTTP_RESPONSE = b"""HTTP/1.1 200 OK
 </html>
 """
 
+class Request:
+    def __init__(self, request):
+        request = request.decode("utf-8")
+
+        header, body = request.split("\r\n\r\n", 1)
+        request_line, *header_list = header.split("\r\n")
+
+        self.request_line = request_line        
+        self.headers = Request.parse_headers(header_list)
+        self.body = body
+
+    @staticmethod
+    def parse_headers(header_list):
+        headers = {}
+        for header in header_list:
+            key, value = header.split(":", 1)
+            headers[key.lower()] = value.lstrip()
+        return headers
+
 def handle_request(conn, addr):
-    request = conn.recv(HEADER_SIZE)
+    request = Request(conn.recv(HEADER_SIZE))
     with conn:
         conn.sendall(HTTP_RESPONSE)
 
