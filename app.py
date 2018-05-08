71bb023f29a6344b627a6c7468fe00718733a949
Jesse Li <jessejesse123@gmail.com>
2018-05-08 16:25:02 -0400

    refactor server code into a class

---
+++ app.py
@@ -0,0 +1,4 @@
+from server import Server
+
+app = Server()
+app.start()
--- server.py
+++ server.py
@@ -2,34 +2,38 @@ import socket
 from threading import Thread
 from plumbing import Request
 
-HOST = ""
-PORT = 5000
-HEADER_SIZE = 1024
 
-def handle_request(conn, addr):
-    request = Request(conn.recv(HEADER_SIZE))
-    with conn:
-        HTTP_RESPONSE = f"""HTTP/1.1 200 OK
+class Server:
+    def __init__(self, port=5000, host="", header_size=1024):
+        self.port = port
+        self.host = host
+        self.header_size = header_size
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            sock.bind((self.host, self.port))
+            sock.listen(1)
+            print(f"Listening on http://{socket.getfqdn()}:{self.port}/")
+            while True:
+                conn, addr = sock.accept()
+                Thread(target=self.handle_request, args=(conn, addr)).start()
+
+    def handle_request(self, conn, addr):
+        request = Request(conn.recv(self.header_size))
+        with conn:
+            response = f"""HTTP/1.1 200 OK
 
 <!DOCTYPE html>
 <html>
   <h1>Your request:</h1>
-  <ol>
+  <ul>
     <li>method: {request.method}</li>
     <li>uri: {request.uri}</li>
     <li>http_version: {request.http_version}</li>
     <li>headers: {request.headers}</li>
     <li>body: {request.body}</li>
-  </ol>
+  </ul>
 </html>
 """
-        conn.sendall(HTTP_RESPONSE.encode("utf-8"))
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind((HOST, PORT))
-    sock.listen(1)
-    print(f"Listening on http://{socket.getfqdn()}:{PORT}/")
-    while True:
-        conn, addr = sock.accept()
-        Thread(target=handle_request, args=(conn, addr)).start()
+            conn.sendall(response.encode("utf-8"))
