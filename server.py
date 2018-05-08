ac6565521b0a21c845ddb4c4d00fc10b583ecc05
Jesse Li <jessejesse123@gmail.com>
2018-05-07 22:06:56 -0400

    return debug information to requests

--- server.py
+++ server.py
@@ -5,13 +5,6 @@ HOST = ""
 PORT = 5000
 HEADER_SIZE = 1024
 
-HTTP_RESPONSE = b"""HTTP/1.1 200 OK
-
-<html>
-  <p>this thing <em>works</em>!</p>
-</html>
-"""
-
 class Request:
     def __init__(self, request):
         request = request.decode("utf-8")
@@ -42,7 +35,21 @@ class Request:
 def handle_request(conn, addr):
     request = Request(conn.recv(HEADER_SIZE))
     with conn:
-        conn.sendall(HTTP_RESPONSE)
+        HTTP_RESPONSE = f"""HTTP/1.1 200 OK
+
+<!DOCTYPE html>
+<html>
+  <h1>Your request:</h1>
+  <ol>
+    <li>method: {request.method}</li>
+    <li>uri: {request.uri}</li>
+    <li>http_version: {request.http_version}</li>
+    <li>headers: {request.headers}</li>
+    <li>body: {request.body}</li>
+  </ol>
+</html>
+"""
+        conn.sendall(HTTP_RESPONSE.encode("utf-8"))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
