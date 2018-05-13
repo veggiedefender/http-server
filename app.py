b4e199fca634fa1245f0824ef45734ecaa799ef9
Jesse Li <jessejesse123@gmail.com>
2018-05-13 18:17:49 -0400

    demo POST abilities

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from server import Server
+from urllib.parse import parse_qs
 
 app = Server()
 
@@ -6,21 +7,35 @@ app = Server()
 def index(request, response):
     response.body = f"""<!DOCTYPE html>
 <html>
-  <h1>Your request:</h1>
-  <ul>
-    <li>method: {request.method}</li>
-    <li>path: {request.path}</li>
-    <li>http_version: {request.http_version}</li>
-    <li>headers: {request.headers}</li>
-    <li>body: {request.body}</li>
-    <li>queries: {request.queries}</li>
-  </ul>
+<head>
+  <title>greeter</title>
+</head>
+<body>
+  <form action="/greet" method="POST">
+    <label for="name">
+      Enter your name:
+      <input type="text" name="name">
+    </label>
+    <input type="submit">
+  </form>
+</body>
 </html>
 """
 
-@app.route("/test")
+@app.route("/greet", methods=["POST"])
 def test(request, response):
-    response.body = "test route!"
+    name = parse_qs(request.body)["name"][0]
+    response.body = f"""<!DOCTYPE html>
+<html>
+<head>
+  <title>hello!</title>
+</head>
+<body>
+  <p>Hello, {name}!</p>
+  <a href="/">try again</a>
+</body>
+</html>
+"""
 
 if __name__ == "__main__":
     app.start(host="0.0.0.0", port=3000)
